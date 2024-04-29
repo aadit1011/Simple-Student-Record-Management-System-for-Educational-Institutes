25de7880da228b2bbc0a742a5a901646757c36a9
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-04-29 22:28:34 +0545

    courses.py

--- courses.py
+++ courses.py
@@ -1,4 +1,4 @@
-#Courses 
+#Courses can be edited and exported as per requirements
 courses_for_civil = ['Structural Engineering', 'Mechanics', 'Hydraulics', 'Mathematics', 'Drawing']
 courses_for_it = ['Applied Operating System', 'Computer Architecture', 'Numerical Methods']
 courses_for_bba = ['Management', 'Economics', 'Finance', 'Communication Techniques', 'Accounting']
