96d84a7e41a1fa64b4c1f7401e82978f7157ecc6
Aadit Sharma <106479910+aadit1011@users.noreply.github.com>
2024-05-21 18:12:09 +0545

    courses.py

--- courses.py
+++ courses.py
@@ -1,5 +1,13 @@
-#Courses can be edited and exported as per requirements
-courses_for_civil = ['Structural Engineering', 'Mechanics', 'Hydraulics', 'Mathematics', 'Drawing']
-courses_for_it = ['Applied Operating System', 'Computer Architecture', 'Numerical Methods']
-courses_for_bba = ['Management', 'Economics', 'Finance', 'Communication Techniques', 'Accounting']
-courses_for_computer = ['Applied Operating System', 'Computer Architecture', 'Numerical Methods', 'Programming in Java']
+#List of courses can be added and changed according to the user requirements
+# List of courses for Civil Engineering department
+courses_for_civil = ['Structural Engineering', 'Mechanics', 'Hydraulics', 'Mathematics', 'Drawing'];
+
+# List of courses for Information Technology department
+courses_for_it = ['Applied Operating System', 'Computer Architecture', 'Numerical Methods'];
+
+# List of courses for Business Administration department
+courses_for_bba = ['Management', 'Economics', 'Finance', 'Communication Techniques', 'Accounting'];
+
+# List of courses for Computer Science department
+courses_for_computer = ['Applied Operating System', 'Computer Architecture', 'Numerical Methods', 'Programming in Java'];
+
